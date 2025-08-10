01f1be0e7200677d0cd960a0fbe4587624f818f4
KseniaZaytseva19 <ksenia.zaytseva19@gmail.com>
2025-08-10 15:35:23 +0300

    Add buttons and file opening function

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout,
-                             QMessageBox, QListWidget, QLineEdit)
+                             QMessageBox, QListWidget, QLineEdit, QFileDialog)
+import os
 
-
+workdir_path = ''
 app = QApplication([])
 win = QWidget()
 win.setFixedSize(700, 500)
 win.setWindowTitle('Editor')
 
 layout1 = QVBoxLayout()
-folder_button = QPushButton('Folder')
+folder_button = QPushButton('Open folder')
 pictures_list = QListWidget()
 layout1.addWidget(folder_button)
 layout1.addWidget(pictures_list)
@@ -26,16 +27,37 @@ layout2.addWidget(mirror_button)
 layout2.addWidget(sharpness_button)
 layout2.addWidget(gray_button)
 
+layout4 = QHBoxLayout()
+vignite_button = QPushButton('Vignite')
+blur_button = QPushButton('Blur')
+contrast_button = QPushButton('Contrast')
+save_button = QPushButton('Save')
+save_button.setStyleSheet("background-color: green; color: white;")
+layout4.addWidget(vignite_button)
+layout4.addWidget(blur_button)
+layout4.addWidget(contrast_button)
+layout4.addWidget(save_button)
+
 layout3 = QVBoxLayout()
 picture = QLabel('Picture')
 layout3.addWidget(picture)
 layout3.addLayout(layout2)
+layout3.addLayout(layout4)
 
 layout_main = QHBoxLayout()
 layout_main.addLayout(layout1)
 layout_main.addLayout(layout3)
 
 win.setLayout(layout_main)
+
+def open_folder():
+    global workdir_path
+    workdir_path = QFileDialog.getExistingDirectory()
+    filenames = os.listdir(workdir_path)
+
+    print(filenames)
+
+folder_button.clicked.connect(open_folder)
 
 win.show()
 app.exec()
