38fbb99484710bf1cbb9b94ff80c9baa46dcf2b1
KseniaZaytseva19 <ksenia.zaytseva19@gmail.com>
2025-08-12 21:00:18 +0300

    Add functions to a class, connecting buttons to functions

--- main.py
+++ main.py
@@ -1,7 +1,62 @@
 from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout,
                              QMessageBox, QListWidget, QLineEdit, QFileDialog)
+from PyQt6.QtGui import QPixmap
 import os
+from PIL import ImageOps, ImageFilter, Image, ImageEnhance
+from PIL.ImageQt import ImageQt
+
+class ImageProcessor():
+    def __init__(self):
+        self.image = None
+        self.filename = ''
+        self.filepath = ''
+
+    def open(self, filename):
+        self.filepath = os.path.join(workdir_path, filename)
+        self.filename = filename
+        self.image = Image.open(self.filepath)
+
+    def show_image(self):
+        q_image = ImageQt(self.image)
+        pixmap = QPixmap.fromImage(q_image)
+        height, width = picture.height(), picture.width()
+        pixmap = pixmap.scaled(width, height, Qt.AspectRatioMode.KeepAspectRatio)
+        picture.setPixmap(pixmap)
+
+    def left(self):
+        left_pic = self.image.rotate(90)
+        self.image = left_pic
+        self.show_image()
+
+    def right(self):
+        self.image = self.image.rotate(270)
+        self.show_image()
+
+    def mirror(self):
+        self.image = ImageOps.mirror(self.image)
+        self.show_image()
+
+    def sharpen(self):
+        self.image = self.image.filter(ImageFilter.SHARPEN)
+        self.show_image()
+
+    def gray(self):
+        self.image = ImageOps.grayscale(self.image)
+        self.show_image()
+
+    def blur(self):
+        self.image = self.image.filter(ImageFilter.BoxBlur(1))
+        self.show_image()
+
+    def contrast(self):
+        self.image = ImageEnhance.Contrast(self.image)
+        self.image = self.image.enhance(1.5)
+        self.show_image()
+
+    def save(self):
+        save_path = QFileDialog.getSaveFileName()
+        self.image.save(save_path[0])
 
 workdir_path = ''
 app = QApplication([])
@@ -28,7 +83,7 @@ layout2.addWidget(sharpness_button)
 layout2.addWidget(gray_button)
 
 layout4 = QHBoxLayout()
-vignite_button = QPushButton('Vignite')
+vignite_button = QPushButton('Vignette')
 blur_button = QPushButton('Blur')
 contrast_button = QPushButton('Contrast')
 save_button = QPushButton('Save')
@@ -50,14 +105,36 @@ layout_main.addLayout(layout3)
 
 win.setLayout(layout_main)
 
+impr = ImageProcessor()
+
 def open_folder():
     global workdir_path
     workdir_path = QFileDialog.getExistingDirectory()
     filenames = os.listdir(workdir_path)
+    filter_filenames(filenames)
+    pictures_list.addItems(filenames)
 
-    print(filenames)
+def filter_filenames(f):
+    for name in f:
+        check = name.endswith(('.png', '.jpg', '.jpeg', '.bmp'))
+        if not check:
+            f.remove(name)
+
+def show():
+    filename = pictures_list.selectedItems()[0].text()
+    impr.open(filename)
+    impr.show_image()
 
 folder_button.clicked.connect(open_folder)
+pictures_list.itemClicked.connect(show)
+left_button.clicked.connect(impr.left)
+right_button.clicked.connect(impr.right)
+mirror_button.clicked.connect(impr.mirror)
+sharpness_button.clicked.connect(impr.sharpen)
+gray_button.clicked.connect(impr.gray)
+blur_button.clicked.connect(impr.blur)
+contrast_button.clicked.connect(impr.contrast)
+save_button.clicked.connect(impr.save)
 
 win.show()
 app.exec()
